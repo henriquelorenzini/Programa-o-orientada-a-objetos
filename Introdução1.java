6aa611aae97908d4c6edb4e06c04c5d8875c3136
henriquelorenzini <38055951+henriquelorenzini@users.noreply.github.com>
2018-08-01 21:10:42 -0300

    Create Introdução1.java
    Introdução a java, apenas imprime uma frase na tela.

---
+++ Introdução1.java
@@ -0,0 +1,12 @@
+// Introducao1.java
+// Apenas imprime um texto na tela
+
+public class Introducao1
+{
+  // método principal inicia a execução do aplicativo Java
+  public static void main(String[] args)
+  {
+    // instrução termina com ; 
+    System.out.println("Primeiro programa em JAVA !");
+  } // fim do main
+}fim da classe Introducao1
