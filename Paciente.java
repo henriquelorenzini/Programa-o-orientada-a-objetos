2bf0b347cd2e38d22c0e3a6a947171be83905a08
henriquelorenzini <38055951+henriquelorenzini@users.noreply.github.com>
2018-08-22 19:42:30 -0300

    Update Paciente.java

--- Paciente.java
+++ Paciente.java
@@ -3,46 +3,38 @@ public class Paciente{
 	public static void main(String[] args){
 		Scanner entrada = new Scanner(System.in);
 		
-		 PerfilMedico paciente = new PerfilMedico(nome, sobrenome, sexo, dia, mes, ano);
-		
-		
 		System.out.printf("Nome: ");
 		String nome = entrada.nextLine();
-		perfil.setNome(nome);
 		
 		System.out.printf("Sobrenome: ");
 		String sobrenome = entrada.nextLine();
-		perfil.setSobrenome(sobrenome);
 		
 		System.out.printf("Sexo: ");
 		String sexo= entrada.nextLine();
-		perfil.setSexo(sexo);
 		
 		System.out.printf("Peso: ");
-		Double peso = entrada.nextFloat();
-		perfil.setPeso(peso);
+		Double peso = entrada.nextDouble();
 		
 		System.out.printf("Altura: ");
-		Double altura = entrada.nextFloat();
-		perfil.setAltura(altura);
+		Double altura = entrada.nextDouble();
 		
 		System.out.printf("Dia do nascimento: ");
 		int dia = entrada.nextInt();
-		perfil.setDia(dia);
 		
 		System.out.printf("Mes do nascimento: ");
 		int mes = entrada.nextInt();
-		perfil.setMes(mes);
 		
 		System.out.printf("Ano do nascimento: ");
 		int ano = entrada.nextInt();
-		perfil.setAno(ano);
+
+		PerfilMedico perfil = new PerfilMedico(nome, sobrenome, dia, mes, ano, sexo, peso, altura);
+
 		
-		System.out.printf("%nNome: %d", perfil.getNome());
-		System.out.printf("%nSobrenome: %d", perfil.getSobrenome());
-		System.out.printf("%nSexo: %d", perfil.getSexo());
+		System.out.printf("%nNome: %s", perfil.getNome());
+		System.out.printf("%nSobrenome: %s", perfil.getSobrenome());
+		System.out.printf("%nSexo: %s", perfil.getSexo());
 		System.out.printf("%nIdade: %d", perfil.getIdade());
-		System.out.printf("Imc: %d", perfil.getImc());
+		System.out.printf("%nImc: %.1f", perfil.getImc());
 
 		entrada.close();
 	}
