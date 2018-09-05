ecfb80806dad99bde2d895d2bc512e1e9b198d08
henriquelorenzini <38055951+henriquelorenzini@users.noreply.github.com>
2018-09-05 18:50:13 -0300

    Update Carro.java

--- Carro.java
+++ Carro.java
@@ -1,33 +1,32 @@
+
 import java.util.Scanner;
 
 public class Carro {
-	public static void main(String[] args){
+
+    public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         int repetir;
-		int totalKm = 0;
-		int totalgas = 0;
-        boolean fim = false;
-		double total;
-        while(!fim) {
-        	System.out.println("Informe a quilometragem percorrida: ");
-        	int Km = entrada.nextInt();
-        	
+        int totalKm = 0;
+        int totalGas = 0;
+        double total;
 
-        	System.out.println("Informe a quantidade de combustivel consumida: ");
-        	int gas = entrada.nextInt();
-        	
-        	totalKm += Km;
-        	totalgas += gas;
-        	total = totalKm/totalgas;
-        	
-        	System.out.println("Quilometragem total: " + totalKm);
-        	System.out.println("Litragem total: " + totalgas);
-        	System.out.println("Rendimento: " + Math.floor(total * 100) / 100);
-        	
-        	System.out.println("Deseja calcular novamente?  Sim - 0 | Nao - 1");
-        	repetir = entrada.nextInt();
-        	fim = (repetir == 1) ? true : false;
-        	
+        System.out.print("Informe a quilometragem percorrida ou digite -1 para terminar: ");
+        int km = entrada.nextInt();
+
+        while (km >= 0) {
+
+            System.out.print("Informe a quantidade de combustivel consumida: ");
+            int gas = entrada.nextInt();
+
+            totalKm += km;
+            totalGas += gas;
+            total = totalKm / totalGas;
+
+            System.out.println("Quilometragem total: " + totalKm);
+            System.out.println("Litragem total: " + totalGas);
+            System.out.println("Rendimento desta viagem: " + Math.floor(total * 100) / 100);
+            System.out.print("Informe a quilometragem percorrida ou digite -1 para terminar: ");
+             km = entrada.nextInt();
         }
-	}
+    }
 }
